9c007d6f4cdf87c9ea3749e4818d993155649da2
Deep15125 <deepgupta15125@gmail.com>
2024-05-06 20:39:01 +0530

    Guess Game Between 1 to 100

---
+++ Task6.py
@@ -0,0 +1,16 @@
+import random
+
+random_number = random.randint(1, 100)
+
+Guess = int(input("Enter any number :"))
+
+while random_number != Guess:
+    if Guess > random_number:
+        print("Your Guess is too High")
+        Guess = int(input("Please Re-enter the number:"))
+    elif Guess < random_number:
+        print("Your Guess is too Low")
+        Guess = int(input("Please Re-enter the number:"))
+    else:
+        break
+print("Congrates! You Guess the Correct Number")
