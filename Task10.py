c6243243be91dbe183f8b93485f056c9ac8ada48
Deep15125 <deepgupta15125@gmail.com>
2024-05-07 19:56:02 +0530

    This is File Manipulation Project

---
+++ Task10.py
@@ -0,0 +1,22 @@
+File = open("Task10.txt" , "r")
+
+d = dict()
+
+for line in File:
+    line = line.strip()
+    line = line.lower()
+
+    words = line.split(" ")
+
+    for word in words:
+        if word in d:
+            d[word] = d[word] + 1
+        else:
+            d[word] = 1
+
+    #Also Print like This
+    #print(word , d[word])
+
+for key in list(d.keys()):
+    print(key, ":" , d[key])
+            
